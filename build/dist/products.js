3d8af4194cb4c778236930b1b7459678db6ab408
lopugit <lopudesigns@gmail.com>
2018-06-15 02:43:23 +1000

    build branch

---
+++ build/dist/products.js
@@ -0,0 +1,14 @@
+var products = {
+    "Highcharts": {
+    "date": "",
+    "nr": "6.1.0-modified"
+    },
+    "Highstock": {
+        "date": "",
+        "nr": "6.1.0-modified"
+    },
+    "Highmaps": {
+        "date": "",
+        "nr": "6.1.0-modified"
+    }
+}
